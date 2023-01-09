d82fdb2e06d20ba2cbf95fd006ee6174447785de
Sergio Centenera Mayoral <scentmay@gmail.com>
2023-01-09 10:53:35 +0100

    módulo funcional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form, Field } from 'formik'
+import { useState } from 'react'
+import './header.css'
+import './content.css'
+import './article.css'
 
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  const open = url => window.open(url)
+
+
+
+  // con este console.log verificamos que está trayendo las fotos
+  console.log(photos)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: '' }}
+          onSubmit={async values => {
+            //llamar a api de unsplash
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+              headers:{
+                'Authorization': 'Client-ID npRdHBLLBogORMzBjI1mgk13ZtOn1f4EgKSo8MILRyw'
+              }
+            })
+            // transformamos lo que nos devuelve la api en un objeto jason
+            const data = await response.json()
+            setPhotos(data.results)
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Form>
+            <Field name='search' />
+          </Form>
+        </Formik>
+      </header> 
+      <div className='container'>
+        <div className='center'>
+          {photos.map(photo => 
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} />
+              <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+            </article>)}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
